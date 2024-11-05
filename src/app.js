902934c736bd3e339ba872e274d68449d4982834
Csilla Toth <csillaout@gmail.com>
2024-11-05 11:51:47 +0000

    getbyid

--- src/app.js
+++ src/app.js
@@ -5,8 +5,26 @@ const db = require("../db/connection");
 
 //TODO: Create your GET Request Route Below:
 app.get("/restaurants", async (req, res) => {
-  const restaurants = await Restaurant.findAll({});
+  const restaurants = await Restaurant.findAll();
   res.json(restaurants);
 });
 
+app.get("/restaurants/:id", async (request, response) => {
+  const restaurantId = request.params.id;
+  const restaurants = await Restaurant.findByPk(restaurantId);
+  response.json(restaurants);
+});
+
+app.use(express.json);
+app.use(express.urlencoded);
+
+app.post("/restaurants", async (request, response) => {
+  const newRestaurant = await Restaurant.create(request.body);
+});
+
+app.put("/restaurants/:id", async (request, response) => {
+  const restaurantId = request.params.id;
+  let restaurant = await Restaurant.findByPk(restaurantId);
+  restaurant = await restaurant.update(request.body);
+});
 module.exports = app;
